43e295df1e385dfe6aa138cb484be70b1cc28a8f
Mario Cruz <mario@mariocruz.net>
2019-07-09 13:02:46 -0400

    Working on MakerSpace Guide

--- src/data.js
+++ src/data.js
@@ -4,22 +4,6 @@ export const credits = [{
 }, {
   name: 'MauroMadeIt',
   link: 'http://mauromadeit.com/',
-  /*
-}, {
-  name: 'Code For Miami',
-  link: 'http://codefor.miami/',
-}, {
-  name: 'Chris Scott',
-}, {
-  name: 'Beacon Council\n(Christine Jones)',
-  link: 'https://www.beaconcouncil.com/',
-}, {
-  name: 'Contexto',
-  link: 'http://www.contexto.co/',
-}, {
-  name: 'Venture Cafe',
-  link: 'http://venturecafemiami.org/',
-  */
 }]
 
 export default {
